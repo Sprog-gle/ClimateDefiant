706c741a28fd544c143470b8e7f235c1703852ac
Samuel Hunt <samuel-hunt@live.com>
2016-08-24 16:54:20 +1200

    Added auth token

--- js/trending.js
+++ js/trending.js
@@ -7,13 +7,15 @@
             url: "https://api.cognitive.microsoft.com/bing/v5.0/news/trendingtopics?" + $.param(params),
             beforeSend: function(xhrObj){
                 // Request headers
-                xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key","{subscription key}");
+                xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key","{5e0807dc266b4f90ba19bc22898e541a}");
                 xhrObj.setRequestHeader("count", "5" );
                 xhrObj.setRequestHeader("headlineCount", "4");
             },
             type: "GET",
             // Request body
             data: "{body}",
+            crossDomain: true,
+            dataType: 'jsonp',
         })
         .done(function(data) {
             alert("success");
