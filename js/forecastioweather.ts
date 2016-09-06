1d7020ebe680196b1911b3bd9a0d380d093717b4
Samuel Hunt <samuel-hunt@live.com>
2016-09-06 18:16:09 +1200

    Fixing url

--- js/forecastioweather.ts
+++ js/forecastioweather.ts
@@ -161,7 +161,7 @@ function getWeather() {
     console.log("get weather run");
 
   $.ajax({
-            'url': 'https://api.forecast.io/forecast/b9b6583bd4214676a38808a223e86e5a/37.8267,-122.423' + lat + ',' + long,
+            'url': 'https://api.forecast.io/forecast/b9b6583bd4214676a38808a223e86e5a/' + lat + ',' + long,
             crossDomain: true,
             // 'async': false,
             'dataType': "jsonp",
