393f397ed0c337a3aea7ff8dc430e70419c64d75
Sam <samuel-hunt@live.com>
2016-09-09 00:55:36 +1200

    (preset weather icon for now)

--- js/weathericons.js
+++ js/weathericons.js
@@ -1,15 +1,10 @@
  // The weather icons code does not work with typescript, so I have written it in JS itself. 
- var setIcon;
-
-
+ 
   var skycons = new Skycons({"color": "darkgrey"});
 
   // you can add a canvas by it's ID...
-  skycons.set("weatherIcon", Skycons.setIcon);
-
-  if (currentCondition === 'wind') {
-      setIcon = Skycons.RAIN;
-  };
+  skycons.set("weatherIcon", Skycons.RAIN);
+  
 
   // if you're using the Forecast API, you can also supply
   // strings: "partly-cloudy-day" or "rain".
